d49b6592f898b0173dd7c47045470afe1a446c63
Kebigon <git@kebigon.xyz>
2019-09-15 14:39:45 +0900

    Store softwares in the /softwares directory

--- _includes/post.freefilesync.sh
+++ _includes/post.freefilesync.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install FreeFileSync into /opt
-wget -O - {{ site.url }}/FreeFileSync_10.8_Linux.tar.gz | tar -xz -C /opt
+wget -O - {{ site.url }}/softwares/FreeFileSync_10.8_Linux.tar.gz | tar -xz -C /opt
 unzip /opt/FreeFileSync/Resources.zip FreeFileSync.png -d /opt/FreeFileSync
 
 # Create launcher
