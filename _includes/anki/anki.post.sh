9f707a7017a40cd6717146f293a6849d209090ab
Kebigon <git@kebigon.xyz>
2020-09-21 14:31:11 +0900

    Update Anki to 2.1.22, move Low-Key Anki addons to dotfiles

--- _includes/anki/anki.post.sh
+++ _includes/anki/anki.post.sh
@@ -3,20 +3,15 @@ set -e
 
 # Download Anki
 anki_major_version=2.1
-anki_minor_version=15
+anki_minor_version=22
 anki_version=${anki_major_version}.${anki_minor_version}
+
 
 wget -O - https://github.com/ankitects/anki/releases/download/${anki_version}/anki-${anki_version}-linux-amd64.tar.bz2 | tar -xj -C /tmp
 cd /tmp/anki-${anki_version}-linux-amd64
 make install
 cd -
 rm -rf /tmp/anki-${anki_version}-linux-amd64
-
-# Low-Key Anki 2.1
-wget "{{ site.url }}/softwares/Low-Key Anki 2.1.zip" -O "/tmp/Low-Key Anki 2.1.zip"
-mkdir -p /etc/skel/.local/share/Anki2/addons21
-unzip "/tmp/Low-Key Anki 2.1.zip" -d /etc/skel/.local/share/Anki2/addons21
-rm "/tmp/Low-Key Anki 2.1.zip"
 
 # Download Anki Add-on from AnkiWeb
 # $1: Add-on code
@@ -39,12 +34,8 @@ download_anki_addon() {
 
 download_anki_addon 3918629684 "Japanese Support"
 download_anki_addon 900801631 "MorphMan for Anki 21"
-download_anki_addon 1666520655 "MIA Retirement"
+download_anki_addon 1666520655 "Migaku Retirement"
 
 # Update MorphMan configuration
 sed -i "s/'min good sentence length': 2/'min good sentence length': 5/" /etc/skel/.local/share/Anki2/addons21/900801631/morph/config.py
 sed -i "s/'max good sentence length': 8/'max good sentence length': 15/" /etc/skel/.local/share/Anki2/addons21/900801631/morph/config.py
-
-# Cleaning
-find /etc/skel -name __MACOSX -exec rm -rfv "{}" \;
-find /etc/skel -name .DS_Store -exec rm -rfv "{}" \;
